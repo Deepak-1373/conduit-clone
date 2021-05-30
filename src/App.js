597207572b33559a8c19ac46c97ad4c45862e712
Deepak Kamat <dkamat.001@gmail.com>
2021-05-30 20:50:45 +0530

    add routes setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import { Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import './styles/App.css';
-import Header from './components/Header';
-import Banner from './components/Banner';
-import Feeds from './components/Feeds';
-import Footer from './components/Footer';
+import "./styles/App.css";
+import Header from "./components/Header";
+import Banner from "./components/Banner";
+import Feeds from "./components/Feeds";
+import Footer from "./components/Footer";
 
 function App() {
-	return (
-		<div className="app">
-			<Router>
-				<Header />
-				<Banner />
-				<Feeds />
-				<Footer />
-			</Router>
-		</div>
-	);
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route to="/" component={Header} />
+          <Route to="/banner" component={Banner} />
+          <Route to="/feeds" component={Feeds} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;

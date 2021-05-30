d5bd3742ad4ebd2bc747362fbf20a4a3f6958b65
rohan-04 <mathurrohan04@gmail.com>
2021-05-30 20:31:02 +0530

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import "./styles/App.css";
-import Header from "./components/Header";
-import Banner from "./components/Banner";
-import Feeds from "./components/Feeds";
-import Footer from "./components/Footer";
+import { Router, Route, Switch } from 'react-router-dom';
+
+import './styles/App.css';
+import Header from './components/Header';
+import Banner from './components/Banner';
+import Feeds from './components/Feeds';
+import Footer from './components/Footer';
 
 function App() {
-  return (
-    <div className="app">
-      <Header />
-      <Banner />
-      <Feeds />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="app">
+			<Router>
+				<Header />
+				<Banner />
+				<Feeds />
+				<Footer />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
